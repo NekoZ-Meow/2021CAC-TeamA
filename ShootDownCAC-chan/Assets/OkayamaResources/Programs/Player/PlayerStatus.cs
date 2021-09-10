88d50e824650b2dda1f24d3b462ddcd8ae0fd964
NekoZ-Meow <okc5424938d474w@gmail.com>
2021-09-10 17:30:29 +0900

    一時保存

---
+++ ShootDownCAC-chan/Assets/OkayamaResources/Programs/Player/PlayerStatus.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatus : MonoBehaviour
+{
+    [SerializeField] private float hp { get; set; } = 0;
+    [SerializeField] private float moveSpeed { get; set; } = 0.1f;
+    [SerializeField] private Shooting shooting { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.shooting = new DoubleNormalShooting(this.gameObject, new NormalBullet());
+    }
+}
