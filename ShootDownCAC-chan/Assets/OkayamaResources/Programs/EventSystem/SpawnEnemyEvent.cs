015ac7209fb93d9a4fc01bd890cc3536d740e071
NekoZ-Meow <okc5424938d474w@gmail.com>
2021-09-17 20:00:16 +0900

    敵生成イベント修正

--- ShootDownCAC-chan/Assets/OkayamaResources/Programs/EventSystem/SpawnEnemyEvent.cs
+++ ShootDownCAC-chan/Assets/OkayamaResources/Programs/EventSystem/SpawnEnemyEvent.cs
@@ -2,49 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnEnemyEvent : Event
-{
-    private GameObject enemy = Resources.Load<GameObject>("Enemy/TestEnemy");
-    private float speed;
-    public SpawnEnemyEvent(string name, float speed = 1.5f)
-    {
-        base.eventName = name;
-        this.speed = speed;
-
-        return;
-    }
-
-    public override IEnumerator Routine()
-    {
-        BoxArea playableArea = AreaUtility.GetPlayableArea();
-        for (int i = 0; i < 10; i++)
-        {
-            float x = Random.Range(playableArea.TopLeft.x, playableArea.BottomRight.x);
-            Vector2 position = new Vector2(x, 6);
-            GameObject enemy = GameObject.Instantiate(this.enemy, position, Quaternion.identity);
-            enemy.GetComponent<EnemyMove>().Speed = this.speed;
-            yield return new WaitForSeconds(1);
-        }
-
-        yield break;
-    }
-}
-
-
+/// <summary>
+/// 直線状に並べて敵を生成する
+/// </summary>
 public class StraightEnemySpawnEvent : Event
 {
-    private GameObject enemy = Resources.Load<GameObject>("Enemy/TestEnemy");
+    private GameObject enemy;
     private int enemyNumber;
     private float direction;
     private Vector2 spawnPosition;
     private float speed;
-    public StraightEnemySpawnEvent(string name, int enemyNumber, float direction, Vector2 spawnPosition, float speed = 1.5f)
+    private float space;
+
+    public StraightEnemySpawnEvent(GameObject enemy, int enemyNumber, float direction, Vector2 spawnPosition, string name = "直線敵生成", float speed = 1.5f, float space = 1.5f)
     {
         base.eventName = name;
         this.enemyNumber = enemyNumber;
         this.direction = direction;
         this.spawnPosition = spawnPosition;
         this.speed = speed;
+        this.space = space;
+        this.enemy = enemy;
         return;
     }
 
@@ -53,9 +31,9 @@ public class StraightEnemySpawnEvent : Event
         List<GameObject> enemies = new List<GameObject>();
         BoxArea playableArea = AreaUtility.GetPlayableArea();
         float rad = direction * Mathf.Deg2Rad;
-        float diffX = 1.5f * Mathf.Cos(rad);
-        float diffY = 1.5f * Mathf.Sin(rad);
-        for (int i = 0; i < 5; i++)
+        float diffX = this.space * Mathf.Cos(rad);
+        float diffY = this.space * Mathf.Sin(rad);
+        for (int i = 0; i < this.enemyNumber; i++)
         {
             GameObject enemy = GameObject.Instantiate(this.enemy, new Vector2(this.spawnPosition.x - i * diffX, spawnPosition.y - i * diffY), Quaternion.identity);
             enemy.GetComponent<EnemyMove>().Speed = this.speed;
